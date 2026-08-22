541497ce47e0b4668b13576cc97c5f99c0725f37
ObrantoApp <obuild.app2026@gmail.com>
2026-08-22 18:13:35 +0200

    Add files via upload

---
+++ GWA/formspree-config.js
@@ -0,0 +1,15 @@
+/*
+  GWA QuickScan - Formspree configuration
+
+  Replace ONLY the value below with the endpoint shown in:
+  Formspree -> GWA QuickScan -> Settings
+
+  Example:
+  https://formspree.io/f/xxxxxxxx
+
+  The current Formspree endpoint was not present in the supplied files,
+  so it is deliberately NOT fabricated here.
+*/
+const FORMSPREE_CONFIG = {
+  endpoint: "https://formspree.io/f/VERVANG_DIT_MET_JOUW_FORM_ID"
+};
